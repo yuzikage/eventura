8de58cd7f299a16719c3c3a571c1b7058bddc123
Smit Sabalpara <130885468+yuzikage@users.noreply.github.com>
2026-04-22 23:14:42 +0530

    App file update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ export default function MyComponent() {
     return (
         <div>
             <h1>Welcome to Eventura!</h1>
+            <h2>An Events Studio based in Coochbehar, West Bengal</h2>
         </div>
     )
 }
